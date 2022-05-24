4861cef29d1d30ded37ad945354dc4a50f5819af
cquentin26200 <qcheron@edenschool.fr>
2022-05-24 17:00:09 +0200

    add function for show all letter

--- vite-project/main.js
+++ vite-project/main.js
@@ -11,12 +11,53 @@ while (i <= 90) {
   i++;
 }*/
 
-function getAlphabet () {
-  for (let i = 97; i <= 122; i++) {
-    for (let o = 65; o <= 90; o++) {
-      console.log(String.fromCharCode(i), String.fromCharCode(o));
+function getAlphabet() {
+  let tmp = [];
+  for (let i = 65; i <= 122; i++) {
+    if (i === 91) {
+      i = 97;
     }
+    tmp.push(String.fromCharCode(i));
   }
+  return tmp.join("");
 }
 
 getAlphabet();
+
+/*const wayOne = document.querySelector(".container");
+const wayTwo = document.getElementById("#app");
+const wayFour = document.querySelector("div#app");
+const wayFive = document.querySelector("div#app.container");
+const waySix = document.querySelector("div.container#app");
+const waySeven = document.querySelector(".container#app");
+const wayHeight = document.querySelector("#app.container");
+const wayNine = document.querySelector("div");*/
+
+const wayThree = document.querySelector("div.container");
+
+const input = document.createElement("input");
+input.setAttribute("type", "text");
+input.placeholder = "Saisissez des lettres";
+
+wayThree.appendChild(input);
+
+const div = document.createElement("div");
+
+wayThree.appendChild(div);
+
+const lengthOfInput = document.createElement("p");
+lengthOfInput.setAttribute("class", "numberOfLetter");
+lengthOfInput.style.textAlign = "right";
+
+div.appendChild(lengthOfInput);
+
+input.addEventListener("input", () => {
+  lengthOfInput.innerHTML = input.value.split("").length;
+});
+
+for (let getAlphabets of getAlphabet().split("")) {
+  const showAlphabet = document.createElement("p");
+  showAlphabet.setAttribute("class", "allLetter");
+  div.appendChild(showAlphabet);
+  showAlphabet  .innerHTML = getAlphabets;
+}
