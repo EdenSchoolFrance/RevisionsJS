6e9907ed621918a504404bdd4aeb2e3de8444bcf
Tom <tsanchis@edenschool.fr>
2022-05-24 17:05:34 +0200

    exo html 50/100

--- revisionsJS_23_05/main.js
+++ revisionsJS_23_05/main.js
@@ -1,3 +1,5 @@
+const div1 = document.querySelector("#app");
+
 import "./style.css";
 
 // for (let i = 97; i <= 122; i++) {
@@ -11,19 +13,31 @@ import "./style.css";
 //   console.log(String.fromCharCode(count));
 // }
 
+const divcontainer = document.createElement("div");
+divcontainer.style.display = "flex";
+divcontainer.style.flexWrap = "wrap";
+divcontainer.style.justifyContent = "center";
+
 const getAlphabet = (str) => {
   for (let i = 65; i <= 122; i++) {
     if (i === 91) {
       i = 97;
     }
+    let divltr = document.createElement("div");
+    divltr.classList.add("ltr-div");
+    divltr.textContent = String.fromCharCode([i]);
+    divltr.style.width = "80px";
+    divltr.style.alignItems = "center";
+    divltr.style.lineHeight = "80px";
+    divltr.style.height = "80px";
+    divltr.style.border = "1px solid green";
+    divltr.style.margin = "10px";
+    divcontainer.appendChild(divltr);
     str += String.fromCharCode([i]);
   }
   return str;
 };
 
-console.log(getAlphabet(""));
-
-const div1 = document.querySelector("#app");
 // console.log(div1, "1");
 
 // const div2 = document.getElementById("app");
@@ -49,3 +63,18 @@ const div1 = document.querySelector("#app");
 
 // const div9 = document.querySelectorAll(".container")[0];
 // console.log(div9, "9");
+let input = document.createElement("input");
+input.placeholder = "Saisissez des lettres";
+div1.appendChild(input);
+
+let p = document.createElement("p");
+p.style.margin = "10px 0px 0px 100px";
+div1.appendChild(p);
+
+div1.appendChild(divcontainer);
+
+setInterval(() => {
+  p.textContent = `Length : ${input.value.length}`;
+});
+
+console.log(getAlphabet(""));
