414180dee423f1c909277a2ec23c3dcf43bb1cb9
benji694 <bbouillard@edenschool.fr>
2022-05-24 14:58:45 +0200

    exo2

--- revisionsJS_23_05/main.js
+++ revisionsJS_23_05/main.js
@@ -3,3 +3,8 @@ import "./style.css";
 for (let i = 97; i <= 122; i++) {
   console.log(String.fromCharCode(i));
 }
+let index = 65;
+while (index <= 90) {
+  console.log(String.fromCharCode(index));
+  index++;
+}
