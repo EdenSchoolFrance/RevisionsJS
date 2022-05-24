59e107d354b557e7f06a9c06309c356e742493c5
benji694 <bbouillard@edenschool.fr>
2022-05-24 15:03:02 +0200

    exo3

--- revisionsJS_23_05/main.js
+++ revisionsJS_23_05/main.js
@@ -8,3 +8,15 @@ while (index <= 90) {
   console.log(String.fromCharCode(index));
   index++;
 }
+
+const getAlphabet = () => {
+  let str = "";
+  for (let i = 65; i <= 122; i++) {
+    if (i == 91) {
+      i = 97;
+    }
+    str += String.fromCharCode(i);
+  }
+  console.log(str);
+};
+getAlphabet();
