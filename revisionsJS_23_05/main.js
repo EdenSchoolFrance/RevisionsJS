ec94956ffdd65f22caec02eb2c752c44c5931c3e
RubenLawen <rcrochet@edenschool.fr>
2022-05-24 15:01:44 +0200

    defi done

--- revisionsJS_23_05/main.js
+++ revisionsJS_23_05/main.js
@@ -1,12 +1,13 @@
 import './style.css'
 
 // De "a" à "z" en minuscule
+// dans la consignes vous avez dit afficher donc j'ai décider de faire avec des consoles log
 for(let i = 97 ; i <= 122; i++){
   console.log(String.fromCharCode(i))
 }
 
 // De "A" à "Z" en majuscule
-
+// dans la consignes vous avez dit afficher donc j'ai décider de faire avec des consoles log
 let index = 65;
 while(index <= 90){
   console.log(String.fromCharCode(index))  
@@ -17,12 +18,14 @@ while(index <= 90){
 
 let getAlphabet = () =>{
   // j'ai mit let en pour la varianle finalword car a chaque tour finalword change donc elle ne peut pas être une constante cue qu'une constante ne peut pas changer
-  let finalword = ''
-  for(let i = 65 ; i <= 90; i++){
+  let finalword = '';
+  let i = 65;
+  while(i <= 122){
+    if(i === 91){
+      i += 6;
+    }
     finalword += String.fromCharCode(i)
-  }
-  for(let i = 97 ; i <= 122; i++){
-    finalword += String.fromCharCode(i)
+    i++
   }
   return finalword
 }
