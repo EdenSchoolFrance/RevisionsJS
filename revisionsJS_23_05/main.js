81ebd1b16da14d2fb4425ac3966bb613d6630edc
Tom <tsanchis@edenschool.fr>
2022-05-24 15:07:18 +0200

    function getAlphabet

--- revisionsJS_23_05/main.js
+++ revisionsJS_23_05/main.js
@@ -1,12 +1,25 @@
 import "./style.css";
 
-for (let i = 97; i <= 122; i++) {
-  console.log(String.fromCharCode([i]));
-}
+// for (let i = 97; i <= 122; i++) {
+//   console.log(String.fromCharCode([i]));
+// }
 
-let count = 64;
+// let count = 64;
 
-while (count < 90) {
-  count++;
-  console.log(String.fromCharCode(count));
-}
+// while (count < 90) {
+//   count++;
+//   console.log(String.fromCharCode(count));
+// }
+
+const getAlphabet = () => {
+  let str = "";
+  for (let i = 65; i <= 122; i++) {
+    if (i === 91) {
+      i = 97;
+    }
+    str += String.fromCharCode([i]);
+  }
+  return str;
+};
+
+console.log(getAlphabet());
