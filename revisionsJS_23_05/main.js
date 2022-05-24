26435f69104cfa3c82dd295fcd0ca05a219bac60
RubenLawen <rcrochet@edenschool.fr>
2022-05-24 15:19:50 +0200

    add the fucntion search correctly

--- revisionsJS_23_05/main.js
+++ revisionsJS_23_05/main.js
@@ -1,37 +1,42 @@
-import './style.css'
+import "./style.css";
 
 // De "a" à "z" en minuscule
 // dans la consignes vous avez dit afficher donc j'ai décider de faire avec des consoles log
-for(let i = 97 ; i <= 122; i++){
-  console.log(String.fromCharCode(i))
+for (let i = 97; i <= 122; i++) {
+  console.log(String.fromCharCode(i));
 }
 
 // De "A" à "Z" en majuscule
 // dans la consignes vous avez dit afficher donc j'ai décider de faire avec des consoles log
 let index = 65;
-while(index <= 90){
-  console.log(String.fromCharCode(index))  
-  index++
+while (index <= 90) {
+  console.log(String.fromCharCode(index));
+  index++;
 }
 
-// function getALphabet 
+// function getALphabet
 
-let getAlphabet = () =>{
+let getAlphabet = () => {
   // j'ai mit let en pour la varianle finalword car a chaque tour finalword change donc elle ne peut pas être une constante cue qu'une constante ne peut pas changer
-  let finalword = '';
+  let finalword = "";
   let i = 65;
-  while(i <= 122){
-    if(i === 91){
+  while (i <= 122) {
+    if (i === 91) {
       i += 6;
     }
-    finalword += String.fromCharCode(i)
-    i++
+    finalword += String.fromCharCode(i);
+    i++;
   }
-  return finalword
-}
+  return finalword;
+};
 
-let searchAlphabet = (alphabet, letter)=>{
-  return alphabet.indexOf(letter)
-}
-
-searchAlphabet(getAlphabet(), "A")
+// Je n'ai pas compris si il fallait trouver la lettre donner avec charCodeAt et avec indexof ou si il fallait en chosir un des deux.
+let searchAlphabet = (alphabet, letter) => {
+  alphabet.indexOf(letter);
+  let ascii = letter.charCodeAt();
+  if (ascii <= 90 && ascii >= 65) {
+    return true;
+  } else if (ascii <= 122 && ascii >= 97) {
+    return false;
+  }
+};
