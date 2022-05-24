5dcd28cbeae217b9a8b53b89eb72d62174485298
benji694 <bbouillard@edenschool.fr>
2022-05-24 15:45:09 +0200

    input and value length

--- revisionsJS_23_05/main.js
+++ revisionsJS_23_05/main.js
@@ -3,6 +3,7 @@ import "./style.css";
 for (let i = 97; i <= 122; i++) {
   console.log(String.fromCharCode(i));
 }
+
 let index = 65;
 while (index <= 90) {
   console.log(String.fromCharCode(index));
@@ -20,8 +21,10 @@ const getAlphabet = () => {
   console.log(str);
   return str;
 };
-const alphabet = getAlphabet();
+
 // J'ai stocker le resultat dans une constante car il ne sera jamais modifier.
+const alphabet = getAlphabet();
+
 const majmin = (str, letter) => {
   let index = str.indexOf(letter);
   let code = str.charCodeAt(index);
@@ -33,3 +36,23 @@ const majmin = (str, letter) => {
 };
 
 majmin(alphabet, "j");
+
+// const app = document.getElementById("app");
+// const app = document.getElementsByClassName("container");
+// const app = document.getElementsByTagName("div");
+// const app = document.querySelector("body > div");
+// const app = document.querySelector("body .container");
+// const app = document.querySelector("body #app");
+// const app = document.querySelector("div");
+// const app = document.querySelector(".container");
+const app = document.querySelector("#app");
+
+const input = document.createElement("input");
+input.placeholder = "Saisissez des lettres";
+app.appendChild(input);
+const p = document.createElement("p");
+p.textContent = 0;
+app.appendChild(p);
+input.addEventListener("input", () => {
+  p.textContent = input.value.length;
+});
