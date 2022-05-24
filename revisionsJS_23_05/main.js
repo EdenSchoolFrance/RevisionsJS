8df7b5fe8e75e68f4fe5d8e46919dfe104456105
RubenLawen <rcrochet@edenschool.fr>
2022-05-24 14:39:39 +0200

    getalphabet function done

--- revisionsJS_23_05/main.js
+++ revisionsJS_23_05/main.js
@@ -12,3 +12,19 @@ while(index <= 90){
   console.log(String.fromCharCode(index))  
   index++
 }
+
+// function getALphabet 
+
+
+let getAlphabet = () =>{
+  let finalword = ''
+  for(let i = 65 ; i <= 90; i++){
+    finalword += String.fromCharCode(i)
+  }
+  for(let i = 97 ; i <= 122; i++){
+    finalword += String.fromCharCode(i)
+  }
+  return finalword
+} 
+
+getAlphabet()
