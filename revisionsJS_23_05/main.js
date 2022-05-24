df405d691980b25f7c7a8a8829f66ac16bbf54b2
Lux <92374048+lucas38460@users.noreply.github.com>
2022-05-24 15:06:13 +0200

    first exercice

--- revisionsJS_23_05/main.js
+++ revisionsJS_23_05/main.js
@@ -1,6 +1,9 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+for (let i = 65; i <= 90 ; i++) {
+  console.log(String.fromCharCode([i]));
+}
+
+for (let i = 97 ; i <= 122 ; i++) {
+  console.log(String.fromCharCode([i]));
+}
