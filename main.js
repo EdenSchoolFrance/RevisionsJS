0ac4ceb536ee9f8071166b43ee5343e4d7c53b83
nmartoia <nmartoia@edenschool.fr>
2022-05-24 14:59:51 +0200

    exo1,2,3 finish

--- main.js
+++ main.js
@@ -4,3 +4,22 @@ document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
+for (let i = 97; i < 123; i++) {
+  console.log(String.fromCharCode(i))
+}
+let n = 65
+while(n<91){
+  console.log(String.fromCharCode(n))
+  n++
+}
+function getAlphabet(){
+  const tmb = []
+  for (let i = 65; i < 123; i++) {
+    if(i===91){
+      i=97
+    }
+    tmb.push(String.fromCharCode(i))
+  }
+  return tmb.join("")
+}
+console.log(getAlphabet())
