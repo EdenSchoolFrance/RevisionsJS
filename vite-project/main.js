06bf3346474b3fdeacc79c1adbb0c74419fbcfd1
cquentin26200 <qcheron@edenschool.fr>
2022-05-24 15:19:40 +0200

    create function for show the letter

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,6 +1,6 @@
 import "./style.css";
 
-for (let i = 97; i <= 122; i++) {
+/*for (let i = 97; i <= 122; i++) {
   console.log(String.fromCharCode(i));
 }
 
@@ -9,4 +9,14 @@ let i = 65;
 while (i <= 90) {
   console.log(String.fromCharCode(i));
   i++;
+}*/
+
+function getAlphabet () {
+  for (let i = 97; i <= 122; i++) {
+    for (let o = 65; o <= 90; o++) {
+      console.log(String.fromCharCode(i), String.fromCharCode(o));
+    }
+  }
 }
+
+getAlphabet();
