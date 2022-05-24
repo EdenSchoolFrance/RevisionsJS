c9264381152896a17efcccf3427a81d5abe52fbc
Lux <92374048+lucas38460@users.noreply.github.com>
2022-05-24 15:52:16 +0200

    tri/ mis à propre

--- revisionsJS_23_05/main.js
+++ revisionsJS_23_05/main.js
@@ -1,5 +1,6 @@
 import "./style.css";
-
+let resp1 = "";
+let resp2 = "";
 let div1 = document.querySelector('.container') ;
 let div2 = document.querySelectorAll('.container') ;
 let div3 = document.getElementById('app');
@@ -10,13 +11,21 @@ let div6 = document.getElementsByClassName('.container') ;
 // let div8 = document. ;
 // let div9 = document. ;
 
-// for (let i = 65; i <= 90 ; i++) {
-//   console.log(String.fromCharCode([i]));
-// }
 
-// for (let i = 97 ; i <= 122 ; i++) {
-//   console.log(String.fromCharCode([i]));
-// }
+
+
+
+for (let i = 65; i <= 90 ; i++) {
+  resp1 += String.fromCharCode([i]);
+ }
+
+ for (let i = 97 ; i <= 122 ; i++) {
+   resp2 += String.fromCharCode([i]);
+ }
+console.log (resp1);
+console.log (resp2);
+
+
 
 const getAlphabet = () => {
   let tmp = "";
@@ -29,10 +38,17 @@ const getAlphabet = () => {
   }
   return tmp;
 };
+console.log(getAlphabet());
 // Pour stocker une variable, une constante est appropriée parce qu'on ne changera rien dans celle-ci.
 
+
+
 const dyn = (getAlphabet) => {
-  return getAlphabet();
+  let respM;
+  for (let i = 0; i < getAlphabet().length; i++) {
+    respM = [i];
+  }
+  return respM;
 }
 
-console.log(dyn(getAlphabet));
+console.log(dyn(getAlphabet()));
