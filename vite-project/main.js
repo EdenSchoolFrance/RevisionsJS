b46ef94a3d2307636d188f4e8a5f092aba1e9a49
cquentin26200 <qcheron@edenschool.fr>
2022-05-24 15:08:53 +0200

    add while for show the upper letter

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,5 +1,12 @@
-import './style.css'
+import "./style.css";
 
 for (let i = 97; i <= 122; i++) {
-  console.log(String.fromCharCode(i))
+  console.log(String.fromCharCode(i));
+}
+
+let i = 65;
+
+while (i <= 90) {
+  console.log(String.fromCharCode(i));
+  i++;
 }
