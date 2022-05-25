88bf4efc19d28bf45d3234d3464320c5d236b222
benji694 <bbouillard@edenschool.fr>
2022-05-25 09:45:21 +0200

    show letter and filter

--- revisionsJS_23_05/main.js
+++ revisionsJS_23_05/main.js
@@ -46,13 +46,54 @@ majmin(alphabet, "j");
 // const app = document.querySelector("div");
 // const app = document.querySelector(".container");
 const app = document.querySelector("#app");
-
+const header = document.querySelector("header");
 const input = document.createElement("input");
 input.placeholder = "Saisissez des lettres";
-app.appendChild(input);
+header.appendChild(input);
 const p = document.createElement("p");
 p.textContent = 0;
-app.appendChild(p);
+header.appendChild(p);
 input.addEventListener("input", () => {
   p.textContent = input.value.length;
+  changeAlphabet();
 });
+
+let filtredAlphabet = alphabet.split("");
+
+const changeAlphabet = () => {
+  let inputValue = input.value.split("");
+
+  if (inputValue.length == 0) {
+    filtredAlphabet = alphabet;
+  } else {
+    filtredAlphabet = alphabet.split("").filter((e) => {
+      return inputValue.includes(e);
+    });
+  }
+  showLetters();
+};
+
+const alphabetList = document.createElement("ul");
+alphabetList.classList = "desktop";
+app.appendChild(alphabetList);
+const showLetters = () => {
+  alphabetList.innerHTML = "";
+  for (let i = 0; i < filtredAlphabet.length; i++) {
+    const letter = document.createElement("li");
+    letter.style.display = "inline-block";
+    letter.style.height = "5rem";
+    letter.textContent = filtredAlphabet[i];
+    alphabetList.appendChild(letter);
+  }
+};
+showLetters();
+const changeDisplay = () => {
+  if (window.innerWidth < 576) {
+    alphabetList.classList = "smartphone";
+  } else if (window.innerWidth < 992 && window.innerWidth > 576) {
+    alphabetList.classList = "tablette";
+  } else {
+    alphabetList.classList = "desktop";
+  }
+};
+window.addEventListener("resize", changeDisplay);
