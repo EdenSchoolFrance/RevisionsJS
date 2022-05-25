91bba21b540a16479e64dabced1262441c63800f
Radwane69 <rnougha@edenschool.fr>
2022-05-25 09:46:24 +0200

    finish exo 1, 2, 3

---
+++ revisionJS_23_05/main.js
@@ -0,0 +1,27 @@
+import "./style.css";
+
+document.querySelector("#app").innerHTML = `
+  <h1>salut Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`;
+// for (let i = 97; i <= 122; i++) {
+//   console.log(String.fromCharCode(i));
+// }
+
+// let i = 64;
+// while (i <= 90) {
+//   console.log(String.fromCharCode(i));
+//   i++;
+// }
+
+// function getAlphabet() {
+//   let strong = "";
+//   for (let i = 65; i <= 122; i++) {
+//     if (i == 91) {
+//       i = 97;
+//     }
+//     strong += String.fromCharCode(i);
+//   }
+//   return strong;
+// }
+// console.log(getAlphabet());
